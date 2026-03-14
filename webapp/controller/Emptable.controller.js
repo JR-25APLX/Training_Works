2dd949a7104c6ab34ba73a3b5de7c86b8e55d806
JOEL REJI <joel.reji@applexus.com>
2026-03-14 02:32:14 +0000

    updated

--- webapp/controller/Emptable.controller.js
+++ webapp/controller/Emptable.controller.js
@@ -8,6 +8,12 @@ sap.ui.define([
         onInit() {
             this.oModel = models.createModel("../model/sampledata/Sample3.json");
             this.getView().setModel(this.oModel,"fiMo");
+        },
+        onSelect: function(oEvent) {
+            var _rContext = oEvent.getParameter("rowContext");
+            var _sPath = _rContext.getPath();
+            var _sF = this.getView().byId('f1');
+            _sF.bindElement(_sPath);
         }
     });
 });
