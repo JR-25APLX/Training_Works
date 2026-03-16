c0373084d15a02923669db7ed5bc38e4afa67668
JOEL REJI <joel.reji@applexus.com>
2026-03-16 08:15:45 +0000

    Updation 4

---
+++ webapp/controller/Layout.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "../model/models"
+], (Controller,models) => {
+    "use strict";
+
+    return Controller.extend("com.applexus.task.controller.Layout", {
+        onInit() {
+        }
+            });
+});
